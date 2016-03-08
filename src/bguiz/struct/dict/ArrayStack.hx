df26338aecbf112c28d7a10dd3e20a32b3b929c8
Brendan Graetz <github@bguiz.com>
2016-03-08 16:55:01 +0800

    =BG= Implement stack

---
+++ src/bguiz/struct/dict/ArrayStack.hx
@@ -0,0 +1,24 @@
+package bguiz.struct.dict;
+
+class ArrayStack {
+    public static function push<T>(stack: ArrayDictionary<T>, item: T): Void {
+      stack.data.push(item);
+      return;
+    }
+
+    public static function pop<T>(stack: ArrayDictionary<T>): Null<T> {
+      return stack.data.pop();
+    }
+
+    public static function peek<T>(stack: ArrayDictionary<T>): Null<T> {
+      return stack.data[stack.data.length - 1];
+    }
+
+    public static function isEmpty<T>(stack: ArrayDictionary<T>): Bool {
+      return stack.data.length == 0;
+    }
+
+    public static function size<T>(stack: ArrayDictionary<T>): Int {
+      return stack.data.length;
+    }
+}
