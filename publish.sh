d0cfdd7de5589d8bbe579f1d0946b130b3214c47
Brendan Graetz <github@bguiz.com>
2016-03-29 19:13:46 +0800

    =BG= publish to both haxelib and npm

--- publish.sh
+++ publish.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# compile
+haxe dist.hxml
+
 # zip up the relevant files
 git ls-files | \
   grep -E -v -e '^test' -e '^\.munit' -e '\.git(keep|ignore)$' -e '^.*\.sh' | \
@@ -7,3 +10,7 @@ git ls-files | \
 
 # publish to haxelib
 haxelib submit build/struct.zip
+
+# publish to npm
+node devops/update-package-json-from-haxelib-json.js
+npm publish
